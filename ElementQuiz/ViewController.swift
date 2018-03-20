140aa7dde952e0361fffc45e883b004c8a515978
Russell Brown <russellpbrown@gmail.com>
2018-03-20 08:26:35 -0700

    Initial Commit

---
+++ ElementQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ElementQuiz
+//
+//  Created by Russell Brown on 3/20/18.
+//  Copyright © 2018 Synesthetic Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
